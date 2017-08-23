7a822534fe64127de08eeb970d34032d3112b3b0
hi363138911 <liangya363138911@126.com>
2017-08-23 16:26:32 +0800

    了解ultron

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 'use strict';
-const ws = require('./ws');
+// const ws = require('./ws');
+
+const Ultron = require('./ws/node_modules/ultron' );
+
+var EventEmitter = require('events').EventEmitter; 
+var events = new EventEmitter();
+var ultron = new Ultron(events);
 
 
+function foo(params) {
+    console.log('----------',params);
+}
+ultron
+    .once('foo', foo);
+
+
+
+// events.on('foo', foo);
+// events.on('foo', foo);
+// events.removeListener('foo', foo);
+
+
+events.emit('foo','-x-');
+events.emit('foo');
